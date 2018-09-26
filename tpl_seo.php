16c013399dda20fee25b1a6c22fffd8ba66c32da
Vladimir Samojlov <v-moi@ya.ru>
2018-09-26 23:14:41 +0300

    add single cases and edit seo and vacancies pages

--- tpl_seo.php
+++ tpl_seo.php
@@ -8,6 +8,16 @@ Template Name: SEO
 
 <div class="tpl_seo">
 	<?php get_template_part( 'blocks/page-top', 'default' ); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				Фото
+			</div>
+			<div class="col-md-6">
+				Инфа
+			</div>
+		</div>
+	</div>
 </div>
 
 <?php get_footer(); ?>
--- tpl_vacancies.php
+++ tpl_vacancies.php
@@ -3,3 +3,11 @@
 Template Name: Вакансии
 */
 ?>
+
+<?php get_header(); ?>
+
+<div class="tpl_reviews">
+	<?php get_template_part( 'blocks/page-top', 'default' ); ?>
+</div>
+
+<?php get_footer(); ?>
