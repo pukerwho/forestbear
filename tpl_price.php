9c427d4c88150856a043b26f7d358a2dc95bf774
Vladimir Samojlov <v-moi@ya.ru>
2018-10-05 00:01:43 +0300

    add price tpl

---
+++ tpl_price.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: Цены
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="tpl_price">
+	<div style="background: url(<?php bloginfo('template_url'); ?>/img/bg-bottom.svg) center bottom/105% no-repeat;">
+		<?php get_template_part( 'blocks/page-top', 'default' ); ?>
+	</div>
+</div>
+
+<?php get_footer(); ?>
