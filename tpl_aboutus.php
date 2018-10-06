2faede80146c7bdf65aae7209f288a5076a5a98c
Vladimir Samojlov <v-moi@ya.ru>
2018-10-07 00:06:05 +0300

    add about tpl

---
+++ tpl_aboutus.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: О нас
+*/
+?>
+
+<?php get_header(); ?>
+<div class="tpl_about">
+	<?php get_template_part( 'blocks/page-top', 'default' ); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="aboutus">
+					<div class="aboutus__text lead">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, earum.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
