04c6fbf79b4b31a51b15064628bfdf9cc10441f7
Vladimir Samojlov <v-moi@ya.ru>
2018-10-07 17:43:56 +0300

    edit style for footer

--- footer.php
+++ footer.php
@@ -2,34 +2,30 @@
     <footer>
 			<div class="container">
 				<div class="row">
-					<div class="col-md-4 mb-5">
+					<div class="col-md-3 mb-5">
 						<div class="footer-heading text-uppercase">
 							Контакты
 						</div>
 						<div class="footer-contact">
-							<div class="address flex align-items-center">
-								<i class="fas fa-map-marker-alt"></i>
-								<address class="from-ua">Украина, Харьков</address>
-							</div>
 							<div class="phone flex align-items-center">
 								<i class="fas fa-phone"></i>
-								<a href="tel:380997713997" class="hover-link">+38(099)-77-13-997</a>
+								<a href="tel:380997713997">+38(099)-77-13-997</a>
 							</div>
 							<div class="email flex align-items-center">
 								<i class="fas fa-envelope"></i>
-								<a href="mailto:web.wemodern@gmail.com" class="hover-link">web.wemodern@gmail.com</a>	
+								<a href="mailto:web.wemodern@gmail.com">web.wemodern@gmail.com</a>	
 							</div>
 							<div class="telegram flex align-items-center">
 								<i class="fab fa-telegram-plane"></i>
-								<a href="tg://resolve?domain=web_WeModern" class="hover-link">Telegram</a>
+								<a href="tg://resolve?domain=web_WeModern">Telegram</a>
 							</div>
 							<div class="facebook flex align-items-center">
 								<i class="fab fa-facebook-f"></i>
-								<a href="https://www.facebook.com/wemodern/" class="hover-link">Facebook</a>
+								<a href="https://www.facebook.com/wemodern/">&nbsp;Facebook</a>
 							</div>
 						</div>
 					</div>
-					<div class="col-md-2 offset-md-1 mb-5">
+					<div class="col-md-2 mb-5">
 						<div class="footer-heading text-uppercase">
 							Поддержка
 						</div>
@@ -49,7 +45,7 @@
 							<li><a href="/reviews">Отзывы</a></li>
 						</div>
 					</div>
-					<div class="col-md-3 mb-5">
+					<div class="col-md-5 mb-5">
 						<div class="footer-heading text-uppercase">
 							Рассылка
 						</div>
